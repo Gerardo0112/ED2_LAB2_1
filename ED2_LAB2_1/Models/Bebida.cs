2cd1228ccb553e99c26f177a990dda7b8f899cf5
unknown <gerar0112@gmail.com>
2020-02-04 23:16:47 -0600

    First commit (Creacion de arbol y nodos)

---
+++ ED2_LAB2_1/Models/Bebida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ED2_LAB2_1.Models
+{
+    public class Bebida
+    {
+        string Name { get; set; }
+        string Flavor { get; set; }
+        int Volume { get; set; }
+        string Price { get; set; }
+        string Producer_House { get; set; }
+    }
+}
---
+++ ED2_LAB2_1/Models/Nodo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ED2_LAB2_1.Models
+{
+    public class Nodo
+    {
+        //Hijos
+        public List<Nodo> edges { get; private set; }
+        //Llaves del arbol
+        public List<int> keys { get; private set; }
+        public Nodo parent { get; set; }
+
+        public Nodo(int key)
+        {
+            keys = new List<int>();
+            keys.Add(key);
+            edges = new List<Nodo>();
+        }
+
+        public int Key(int x)
+        {
+            for(int y = 0; ; y < keys.Count; y++) 
+            {
+                if(keys[y] == x)
+                {
+                    return y;
+                }
+            }
+            return -1;
+        }
+    }
+ }
